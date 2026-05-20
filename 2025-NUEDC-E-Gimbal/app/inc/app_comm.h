0028ccd2a11a8ed86038b651480e8d43051f8dd2
洛书 <1342701603@qq.com>
2026-05-20 20:39:35 +0800

    fix(comm): 更新通信模块文档，修正 TI 命令解析为 TI 命令分发，移除无用代码

--- 2025-NUEDC-E-Gimbal/app/inc/app_comm.h
+++ 2025-NUEDC-E-Gimbal/app/inc/app_comm.h
@@ -1,6 +1,6 @@
 /**
  * @file app_comm.h
- * @brief 通信模块 - TI 命令解析 + K230 数据接收
+ * @brief 通信模块 - TI 命令分发 + K230 数据接收
  */
 
 #ifndef __APP_COMM_H_
@@ -12,49 +12,10 @@ extern "C" {
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "ti.h"
 #include "k230.h"
 
 #define APP_COMM_CMD_MAX_LENGTH 64U
-
-/**
- * @brief TI 控制指令类型
- */
-typedef enum {
-    CMD_UNKNOWN = 0,
-    CMD_LASER_ON,
-    CMD_LASER_OFF,
-    CMD_MOTOR0_ENABLE,
-    CMD_MOTOR0_DISABLE,
-    CMD_MOTOR1_ENABLE,
-    CMD_MOTOR1_DISABLE,
-    CMD_MOTOR0_SPEED,
-    CMD_MOTOR1_SPEED,
-    CMD_MOTOR0_ANGLE,
-    CMD_MOTOR1_ANGLE,
-    CMD_LED_RED_ON,
-    CMD_LED_RED_OFF,
-    CMD_LED_GREEN_ON,
-    CMD_LED_GREEN_OFF,
-    CMD_LED_BLUE_ON,
-    CMD_LED_BLUE_OFF,
-    CMD_LED_RED_BLINK,
-    CMD_LED_GREEN_BLINK,
-    CMD_LED_BLUE_BLINK,
-} ti_cmd_type_t;
-
-/**
- * @brief TI 控制指令数据结构
- */
-typedef struct {
-    ti_cmd_type_t type;
-    float value;
-    bool valid;
-} ti_cmd_t;
-
-/**
- * @brief TI 指令回调函数类型
- */
-typedef void (*ti_cmd_callback_t)(const ti_cmd_t *cmd);
 
 /**
  * @brief 初始化通信模块
